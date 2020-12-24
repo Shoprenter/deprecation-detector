55cc650692420e095f28ba4c05578d9caab90a65
carlos granados <carlos@i6.io>
2020-12-24 10:23:30 +0000

    Update PHP Parser version to cover latest versions of PHP

--- src/TypeGuessing/SymbolTable/Resolver/ArgumentResolver.php
+++ src/TypeGuessing/SymbolTable/Resolver/ArgumentResolver.php
@@ -25,8 +25,8 @@ class ArgumentResolver implements ResolverInterface
      */
     public function resolveVariableType(Node $node)
     {
-        if ($node instanceof Node\Param && $node->type instanceof Node\Name) {
-            $this->table->setSymbol($node->name, $node->type->toString());
+        if ($node instanceof Node\Param && $node->type instanceof Node\Name && $node->var instanceof Node\Expr\Variable) {
+            $this->table->setSymbol($node->var->name, $node->type->toString());
         }
     }
 }
